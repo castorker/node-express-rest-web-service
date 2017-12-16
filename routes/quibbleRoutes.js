4bce5bbd489b8cf93c5f93249d34288fcbf7fd79
Eduardo <eduardo.cxcorreio@gmail.com>
2017-12-16 18:09:31 +0100

    Configured Middleware. Implemented HTTP PUT, PATCH and DELETE verbs.

--- routes/quibbleRoutes.js
+++ routes/quibbleRoutes.js
@@ -31,18 +31,68 @@ var routes = function(Quibble) {
       });
     });
 
+  // middleware
+  quibbleRouter.use('/:quibbleId', function (req, res, next) {
+    Quibble.findById(req.params.quibbleId, function (err, quibble) {
+      if (err) {
+        res.status(500).send(err);
+      } else if (quibble) {
+        req.quibble = quibble;
+        next();
+      }
+      else {
+        res.status(404).send('No quibble found.');
+      }
+    });
+  });
+
   quibbleRouter.route('/:quibbleId')
 
     .get(function (req, res) {
+      res.json(req.quibble);
+    })
 
-      Quibble.findById(req.params.quibbleId, function(err, quibble) {
+    .put(function(req, res) {
+      // req.quibble.id = req.body.id;
+      req.quibble.text = req.body.text;
+      req.quibble.category = req.body.category;
+      req.quibble.like = req.body.like;
+      req.quibble.save(function(err) {
         if (err) {
-            res.status(500).send(err);
-          } else {
-            res.json(quibble);
-          }
-        });
+          res.status(500).send(err);
+        } else {
+          res.json(req.quibble);
+        }
       });
+    })
+
+    .patch(function(req, res) {
+      if (req.body._id) {
+        delete req.body._id;
+      }
+
+      for (var p in req.body) {
+        req.quibble[p] = req.body[p];
+      }
+
+      req.quibble.save(function (err) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(req.quibble);
+        }
+      });
+    })
+
+    .delete(function (req, res) {
+      req.quibble.remove(function (err) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.status(204).send('Quibble removed.');
+        }
+    });
+  });
 
   return quibbleRouter;
 };
